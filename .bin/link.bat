babdcf1ee04b3eb54e2a2a914199ba2559e59962
izassan <izassan.si@gmail.com>
2023-05-13 12:13:36 +0900

    fix: fixed a bug that caused creationn of symbolic links to fail when there was no directory

--- .bin/link.bat
+++ .bin/link.bat
@@ -1,5 +1,6 @@
 set CONFIGROOTDIR="%~dp0\.."
 
+mkdir %USERPROFILE%\Documents\PowersShell
 mklink %USERPROFILE%\Documents\PowersShell\Profile.ps1 %CONFIGROOTDIR%\profile.ps1
 
 mklink %USERPROFILE%\vimfiles %CONFIGROOTDIR%\config\vim
