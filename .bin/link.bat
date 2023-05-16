db8416670aacf3b1c7a3bcc526e09682fb83c5b6
izassan <izassan.si@gmail.com>
2023-05-16 21:19:51 +0900

    fix: add mklink option for vimfiles directory

--- .bin/link.bat
+++ .bin/link.bat
@@ -3,5 +3,5 @@ set CONFIGROOTDIR="%~dp0\.."
 mkdir %USERPROFILE%\Documents\PowerShell
 mklink %USERPROFILE%\Documents\PowerShell\Profile.ps1 %CONFIGROOTDIR%\profile.ps1
 
-mklink %USERPROFILE%\vimfiles %CONFIGROOTDIR%\config\vim
+mklink /D %USERPROFILE%\vimfiles %CONFIGROOTDIR%\config\vim
 mklink %USERPROFILE%\.gitconfig %CONFIGROOTDIR%\config\gitconfig
