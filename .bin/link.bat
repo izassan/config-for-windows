fd9d6e2ff8dc5c055bc77bef96b713f8a996e056
izassan <izassan.si@gmail.com>
2023-05-16 21:05:33 +0900

    fix: fix powershell config file path

--- .bin/link.bat
+++ .bin/link.bat
@@ -1,7 +1,7 @@
 set CONFIGROOTDIR="%~dp0\.."
 
-mkdir %USERPROFILE%\Documents\PowersShell
-mklink %USERPROFILE%\Documents\PowersShell\Profile.ps1 %CONFIGROOTDIR%\profile.ps1
+mkdir %USERPROFILE%\Documents\PowerShell
+mklink %USERPROFILE%\Documents\PowerShell\Profile.ps1 %CONFIGROOTDIR%\profile.ps1
 
 mklink %USERPROFILE%\vimfiles %CONFIGROOTDIR%\config\vim
 mklink %USERPROFILE%\.gitconfig %CONFIGROOTDIR%\config\gitconfig
